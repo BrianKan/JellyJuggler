6d97843c85e818e039247eb0e5e08be32e12f0a4
briank <brian.a.kan@gmail.com>
2017-07-14 16:12:46 -0700

    First Commit Menus wip

---
+++ desktop/src/com/redfish/jellyjuggler/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.redfish.jellyjuggler.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.redfish.jellyjuggler.JellyJuggler;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JellyJuggler(), config);
+	}
+}
